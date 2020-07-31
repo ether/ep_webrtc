c2c2aa6869eb73c99734ff51aabbe45b82d0ed50
Daniel Krol <orblivion@gmail.com>
2020-07-31 18:23:00 -0400

    Test ep_webrtc.disabled url var

--- static/tests/frontend/specs/audio_video_on_start.js
+++ static/tests/frontend/specs/audio_video_on_start.js
@@ -1,5 +1,42 @@
-describe('test that audio and video are on or off on start according to urlVars and cookies', function() {
-  context('audio on, video off via cookies', function() {
+describe('test that webrtc, audio and video are on or off on start according to urlVars and cookies', function() {
+  context('webrtc off via cookies', function() {
+    before(function(done) {
+      this.timeout(60000);
+      helper.newPad({
+        padPrefs: {
+          rtcEnabled: false,
+          fakeWebrtcFirefox: true,
+          audioEnabledOnStart: true,
+          videoEnabledOnStart: true,
+        },
+        cb: function () {
+          var chrome$
+          chrome$ = helper.padChrome$;
+          helper.waitFor(function() {
+            chrome$ = helper.padChrome$;
+            return chrome$ && chrome$("#rtcbox").length === 1;
+          }, 1000).done(function() {
+            setTimeout(done, 1000)
+          })
+        }
+      });
+    });
+
+    it('has the expected checkbox values with and without urlVars', function(done) {
+      chrome$ = helper.padChrome$;
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(false)
+
+      // overriding with url params
+      chrome$.window.ep_webrtc.setUrlParamString("?webrtcenabled=true")
+      chrome$.window.ep_webrtc.setupCheckboxes()
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(true)
+
+      done()
+    });
+
+  });
+
+  context('webrtc on, audio on, video off via cookies', function() {
     before(function(done) {
       this.timeout(60000);
       helper.newPad({
@@ -24,18 +61,20 @@ describe('test that audio and video are on or off on start according to urlVars 
       chrome$ = helper.padChrome$;
       expect(chrome$('#options-audioenabledonstart').prop("checked")).to.equal(true)
       expect(chrome$('#options-videoenabledonstart').prop("checked")).to.equal(false)
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(true)
 
       // overriding with url params
       chrome$.window.ep_webrtc.setUrlParamString("?webrtcaudioenabled=false&webrtcvideoenabled=true")
       chrome$.window.ep_webrtc.setupCheckboxes()
       expect(chrome$('#options-audioenabledonstart').prop("checked")).to.equal(false)
       expect(chrome$('#options-videoenabledonstart').prop("checked")).to.equal(true)
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(true)
 
       done()
     });
   });
 
-  context('audio off, video on via cookies, no url params', function() {
+  context('webrtc on, audio off, video on via cookies, no url params', function() {
     before(function(done) {
       this.timeout(60000);
       helper.newPad({
@@ -60,12 +99,14 @@ describe('test that audio and video are on or off on start according to urlVars 
       chrome$ = helper.padChrome$;
       expect(chrome$('#options-audioenabledonstart').prop("checked")).to.equal(false)
       expect(chrome$('#options-videoenabledonstart').prop("checked")).to.equal(true)
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(true)
 
       // overriding with url params
       chrome$.window.ep_webrtc.setUrlParamString("?webrtcaudioenabled=true&webrtcvideoenabled=false")
       chrome$.window.ep_webrtc.setupCheckboxes()
       expect(chrome$('#options-audioenabledonstart').prop("checked")).to.equal(true)
       expect(chrome$('#options-videoenabledonstart').prop("checked")).to.equal(false)
+      expect(chrome$('#options-enablertc').prop("checked")).to.equal(true)
 
       done()
     });
