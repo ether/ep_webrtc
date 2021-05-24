a74315cc6ba5060f9b7243e4154241d25c83b453
Richard Hansen <rhansen@rhansen.org>
2021-05-24 00:53:19 -0400

    tests: Promisify `interface_buttons.js`

--- static/tests/frontend/specs/interface_buttons.js
+++ static/tests/frontend/specs/interface_buttons.js
@@ -1,64 +1,55 @@
-/* eslint max-len: ["error", { "code": 120 }] */
 'use strict';
 
 describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlarge', function () {
   let audioTrack;
   let videoTrack;
 
-  function wrapGetUserMedia() {
+  const wrapGetUserMedia = () => {
     const chrome$ = helper.padChrome$;
     const oldGetUserMedia = chrome$.window.navigator.mediaDevices.getUserMedia;
-    chrome$.window.navigator.mediaDevices.getUserMedia = function (mediaConstraints) {
-      return oldGetUserMedia.call(chrome$.window.navigator.mediaDevices, mediaConstraints)
-          .then((stream) => {
-            audioTrack = stream.getAudioTracks()[0];
-            videoTrack = stream.getVideoTracks()[0];
-            return stream;
-          });
+    chrome$.window.navigator.mediaDevices.getUserMedia = async (constraints) => {
+      const stream = await oldGetUserMedia.call(chrome$.window.navigator.mediaDevices, constraints);
+      audioTrack = stream.getAudioTracks()[0];
+      videoTrack = stream.getVideoTracks()[0];
+      return stream;
     };
-  }
+  };
 
-  context('audio and video on by default', function () {
-    beforeEach(function (done) {
+  describe('audio and video on by default', function () {
+    beforeEach(async function () {
+      this.timeout(60000);
       audioTrack = null;
       videoTrack = null;
 
       // Make sure webrtc starts disabled so we have time to wrap getUserMedia
-      helper.newPad({
+      await helper.aNewPad({
         padPrefs: {
           rtcEnabled: false,
           fakeWebrtcFirefox: true,
           audioEnabledOnStart: true,
           videoEnabledOnStart: true,
         },
-        cb() {
-          const chrome$ = helper.padChrome$;
-
-          helper.waitFor(() => chrome$ &&
-              chrome$('#options-enablertc').length === 1, 2000).done(() => {
-            wrapGetUserMedia();
-
-            const $enableRtc = chrome$('#options-enablertc');
-            $enableRtc.click(); // Turn it on late so that wrapGetUserMedia works
-
-            helper.waitFor(() => (
-              chrome$('.audio-btn').length === 1 &&
-                chrome$('.video-btn').length === 1 &&
-                audioTrack != null &&
-                videoTrack != null
-            ), 1000).done(() => {
-              // Video interface buttons are added twice, and there's no good
-              // way besides a timeout to tell when it's done
-              // being called the second time. We want it to be finished so our test is stable.
-              setTimeout(done, 200);
-            });
-          });
-        },
       });
-      this.timeout(60000);
+      const chrome$ = helper.padChrome$;
+
+      await helper.waitForPromise(
+          () => chrome$ && chrome$('#options-enablertc').length === 1, 2000);
+      wrapGetUserMedia();
+
+      const $enableRtc = chrome$('#options-enablertc');
+      $enableRtc.click(); // Turn it on late so that wrapGetUserMedia works
+
+      await helper.waitForPromise(
+          () => (chrome$('.audio-btn').length === 1 && chrome$('.video-btn').length === 1 &&
+                 audioTrack != null && videoTrack != null),
+          1000);
+      // Video interface buttons are added twice, and there's no good way besides a timeout to tell
+      // when it's done being called the second time. We want it to be finished so our test is
+      // stable.
+      await new Promise((resolve) => setTimeout(resolve, 200));
     });
 
-    it('enlarges then shrinks', function (done) {
+    it('enlarges then shrinks', async function () {
       const chrome$ = helper.padChrome$;
 
       this.timeout(60000);
@@ -79,24 +70,19 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       $enlargeBtn.click();
 
       // Expect it to grow to 260, 190
-      helper.waitFor(() => (
-        numFromCssSize($video.css('width')) > 259 &&
-          numFromCssSize($video.css('height')) > 190
-      ),
-      1000
-      ).done(() => {
-        $enlargeBtn.click();
-        // Expect it to shrink to 160, 116
-        helper.waitFor(() => (
-          numFromCssSize($video.css('width')) < 161 &&
-            numFromCssSize($video.css('height')) < 117
-        ),
-        1000
-        ).done(done);
-      });
+      await helper.waitForPromise(
+          () => (numFromCssSize($video.css('width')) > 259 &&
+                 numFromCssSize($video.css('height')) > 190),
+          1000);
+      $enlargeBtn.click();
+      // Expect it to shrink to 160, 116
+      await helper.waitForPromise(
+          () => (numFromCssSize($video.css('width')) < 161 &&
+                 numFromCssSize($video.css('height')) < 117),
+          1000);
     });
 
-    it('mutes then unmutes', function (done) {
+    it('mutes then unmutes', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
@@ -108,17 +94,16 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       const $audioBtn = chrome$('.audio-btn');
       $audioBtn.click();
 
-      helper.waitFor(() => chrome$('.audio-btn.muted').length === 1 && audioTrack.enabled === false, 3000).done(() => {
-        expect(chrome$('.audio-btn').attr('title')).to.be('Unmute');
-        $audioBtn.click();
-        helper.waitFor(() => chrome$('.audio-btn.muted').length === 0 && audioTrack.enabled === true, 3000).done(() => {
-          expect(chrome$('.audio-btn').attr('title')).to.be('Mute');
-          done();
-        });
-      });
+      await helper.waitForPromise(
+          () => chrome$('.audio-btn.muted').length === 1 && audioTrack.enabled === false, 3000);
+      expect(chrome$('.audio-btn').attr('title')).to.be('Unmute');
+      $audioBtn.click();
+      await helper.waitForPromise(
+          () => chrome$('.audio-btn.muted').length === 0 && audioTrack.enabled === true, 3000);
+      expect(chrome$('.audio-btn').attr('title')).to.be('Mute');
     });
 
-    it('disables then enables video', function (done) {
+    it('disables then enables video', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
@@ -130,56 +115,50 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       const $videoBtn = chrome$('.video-btn');
       $videoBtn.click();
 
-      helper.waitFor(() => chrome$('.video-btn.off').length === 1 && videoTrack.enabled === false, 3000).done(() => {
-        expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
-        $videoBtn.click();
-        helper.waitFor(() => chrome$('.video-btn.off').length === 0 && videoTrack.enabled === true, 3000).done(() => {
-          expect(chrome$('.video-btn').attr('title')).to.contain('Disable');
-          done();
-        });
-      });
+      await helper.waitForPromise(
+          () => chrome$('.video-btn.off').length === 1 && videoTrack.enabled === false, 3000);
+      expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
+      $videoBtn.click();
+      await helper.waitForPromise(
+          () => chrome$('.video-btn.off').length === 0 && videoTrack.enabled === true, 3000);
+      expect(chrome$('.video-btn').attr('title')).to.contain('Disable');
     });
   });
 
   context('audio and video off by default', function () {
-    beforeEach(function (done) {
+    beforeEach(async function () {
+      this.timeout(60000);
       audioTrack = null;
       videoTrack = null;
 
       // Make sure webrtc starts disabled so we have time to wrap getUserMedia
-      helper.newPad({
+      await helper.aNewPad({
         padPrefs: {
           rtcEnabled: false,
           fakeWebrtcFirefox: true,
           audioEnabledOnStart: false,
           videoEnabledOnStart: false,
         },
-        cb() {
-          const chrome$ = helper.padChrome$;
-
-          helper.waitFor(() => chrome$ && chrome$('#options-enablertc').length === 1, 2000).done(() => {
-            wrapGetUserMedia();
-            const $enableRtc = chrome$('#options-enablertc');
-            $enableRtc.click(); // Turn it on late so that wrapGetUserMedia works
-
-            helper.waitFor(() => (
-              chrome$('.audio-btn').length === 1 &&
-                chrome$('.video-btn').length === 1 &&
-                audioTrack != null &&
-                videoTrack != null
-            ), 1000).done(() => {
-              // Video interface buttons are added twice,
-              // and there's no good way besides a timeout to tell when it's done
-              // being called the second time. We want it to be finished so our test is stable.
-              setTimeout(done, 200);
-            });
-          });
-        },
       });
-      this.timeout(60000);
+      const chrome$ = helper.padChrome$;
+
+      await helper.waitForPromise(
+          () => chrome$ && chrome$('#options-enablertc').length === 1, 2000);
+      wrapGetUserMedia();
+      const $enableRtc = chrome$('#options-enablertc');
+      $enableRtc.click(); // Turn it on late so that wrapGetUserMedia works
+
+      await helper.waitForPromise(
+          () => (chrome$('.audio-btn').length === 1 && chrome$('.video-btn').length === 1 &&
+                 audioTrack != null && videoTrack != null),
+          1000);
+      // Video interface buttons are added twice, and there's no good way besides a timeout to tell
+      // when it's done being called the second time. We want it to be finished so our test is
+      // stable.
+      await new Promise((resolve) => setTimeout(resolve, 200));
     });
 
-    it('unmutes then mutes', function (done) {
+    it('unmutes then mutes', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
@@ -191,75 +170,64 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       const $audioBtn = chrome$('.audio-btn');
       $audioBtn.click();
 
-      helper.waitFor(() => chrome$('.audio-btn.muted').length === 0 &&
-          audioTrack.enabled === true, 3000).done(() => {
-        expect(chrome$('.audio-btn').attr('title')).to.be('Mute');
-        $audioBtn.click();
-        helper.waitFor(() => chrome$('.audio-btn.muted').length === 1 &&
-            audioTrack.enabled === false, 3000).done(() => {
-          expect(chrome$('.audio-btn').attr('title')).to.be('Unmute');
-          done();
-        });
-      });
+      await helper.waitForPromise(
+          () => chrome$('.audio-btn.muted').length === 0 && audioTrack.enabled === true, 3000);
+      expect(chrome$('.audio-btn').attr('title')).to.be('Mute');
+      $audioBtn.click();
+      await helper.waitForPromise(
+          () => chrome$('.audio-btn.muted').length === 1 && audioTrack.enabled === false, 3000);
+      expect(chrome$('.audio-btn').attr('title')).to.be('Unmute');
     });
 
-    it('enables then disables video', function (done) {
+    it('enables then disables video', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
 
-      helper.waitFor(() => chrome$('.video-btn').length === 1 && videoTrack != null, 3000).done(() => {
-        expect(videoTrack.enabled).to.be(false);
-        expect(chrome$('.video-btn.off').length).to.be(1);
-        expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
+      await helper.waitForPromise(
+          () => chrome$('.video-btn').length === 1 && videoTrack != null, 3000);
+      expect(videoTrack.enabled).to.be(false);
+      expect(chrome$('.video-btn.off').length).to.be(1);
+      expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
 
-        const $videoBtn = chrome$('.video-btn');
-        $videoBtn.click();
+      const $videoBtn = chrome$('.video-btn');
+      $videoBtn.click();
 
-        helper.waitFor(() => chrome$('.video-btn.off').length === 0 && videoTrack.enabled === true, 3000).done(() => {
-          expect(chrome$('.video-btn').attr('title')).to.contain('Disable');
-          $videoBtn.click();
-          helper.waitFor(() => chrome$('.video-btn.off').length === 1 &&
-              videoTrack.enabled === false, 3000).done(() => {
-            expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
-            done();
-          });
-        });
-      });
+      await helper.waitForPromise(
+          () => chrome$('.video-btn.off').length === 0 && videoTrack.enabled === true, 3000);
+      expect(chrome$('.video-btn').attr('title')).to.contain('Disable');
+      $videoBtn.click();
+      await helper.waitForPromise(
+          () => chrome$('.video-btn.off').length === 1 && videoTrack.enabled === false, 3000);
+      expect(chrome$('.video-btn').attr('title')).to.contain('Enable');
     });
   });
 
   context('audio and video hard-disabled', function () {
-    beforeEach(function (done) {
+    beforeEach(async function () {
+      this.timeout(60000);
       audioTrack = null;
       videoTrack = null;
-
       // Make sure webrtc starts disabled so we have time to wrap getUserMedia
-      helper.newPad({
+      await helper.aNewPad({
         padPrefs: {
           rtcEnabled: true,
           fakeWebrtcFirefox: true,
         },
-        cb() {
-          const chrome$ = helper.padChrome$;
-          chrome$.window.clientVars.webrtc.audio.disabled = 'hard';
-          chrome$.window.clientVars.webrtc.video.disabled = 'hard';
-
-          helper.waitFor(() => (
-            chrome$('.audio-btn').length === 1 &&
-              chrome$('.video-btn').length === 1
-          ), 1000).done(() => {
-            wrapGetUserMedia();
-            // Video interface buttons are added twice, and there's no good way besides a timeout to tell when it's done
-            // being called the second time. We want it to be finished so our test is stable.
-            setTimeout(done, 200);
-          });
-        },
       });
-      this.timeout(60000);
+      const chrome$ = helper.padChrome$;
+      chrome$.window.clientVars.webrtc.audio.disabled = 'hard';
+      chrome$.window.clientVars.webrtc.video.disabled = 'hard';
+      await helper.waitForPromise(
+          () => (chrome$('.audio-btn').length === 1 && chrome$('.video-btn').length === 1), 1000);
+      wrapGetUserMedia();
+      // Video interface buttons are added twice, and there's no good way besides a timeout to tell
+      // when it's done being called the second time. We want it to be finished so our test is
+      // stable.
+      await new Promise((resolve) => setTimeout(resolve, 200));
     });
 
-    it('cannot mute or unmute', function (done) {
+    it('cannot mute or unmute', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
@@ -273,18 +241,16 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       const $audioBtn = chrome$('.audio-btn');
       $audioBtn.click();
 
-      setTimeout(() => {
-        // Wait a sec to make sure there's no change
-        expect(chrome$('.audio-btn.disallowed').length).to.be(1);
-        expect(chrome$('.audio-btn.muted').length).to.be(1);
-        expect(chrome$('.audio-btn').attr('title')).to.be('Audio disallowed by admin');
-        expect(audioTrack).to.be(null);
-        expect(videoTrack).to.be(null);
-        done();
-      }, 200);
+      // Wait a sec to make sure there's no change
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      expect(chrome$('.audio-btn.disallowed').length).to.be(1);
+      expect(chrome$('.audio-btn.muted').length).to.be(1);
+      expect(chrome$('.audio-btn').attr('title')).to.be('Audio disallowed by admin');
+      expect(audioTrack).to.be(null);
+      expect(videoTrack).to.be(null);
     });
 
-    it('cannot enable or disable video', function (done) {
+    it('cannot enable or disable video', async function () {
       this.timeout(60000);
 
       const chrome$ = helper.padChrome$;
@@ -298,15 +264,13 @@ describe('Test the behavior of the interface buttons: Mute, Video Disable, Enlar
       const $videoBtn = chrome$('.video-btn');
       $videoBtn.click();
 
-      setTimeout(() => {
-        // Wait a sec to make sure there's no change
-        expect(chrome$('.video-btn.disallowed').length).to.be(1);
-        expect(chrome$('.video-btn.off').length).to.be(1);
-        expect(chrome$('.video-btn').attr('title')).to.be('Video disallowed by admin');
-        expect(audioTrack).to.be(null);
-        expect(videoTrack).to.be(null);
-        done();
-      }, 200);
+      // Wait a sec to make sure there's no change
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      expect(chrome$('.video-btn.disallowed').length).to.be(1);
+      expect(chrome$('.video-btn.off').length).to.be(1);
+      expect(chrome$('.video-btn').attr('title')).to.be('Video disallowed by admin');
+      expect(audioTrack).to.be(null);
+      expect(videoTrack).to.be(null);
     });
   });
 });
