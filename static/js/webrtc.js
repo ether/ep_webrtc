ether/ep_webrtc

commit 62b1ce8e28bef49bd6b31cbd6405bd0385719a26

lint fix

static/js/webrtc.js
@@ -515,7 +515,7 @@ const rtc = (() => {
     getUserId: () => self._pad && self._pad.getUserId(),
     hangup: (...args) => {
       const userId = args[0];
-      const notify = args.length === 1 ? true : notify;
+      const notify = args[1] || true;
       if (pc[userId] && userId !== self.getUserId()) {
         self.setStream(userId, '');
         pc[userId].close();

commit 729584ec018c57dae48dc7fc2f59d6c6fb643da7

Fix handling of `notify` arg to `hangup()`
This fixes a bug affecting versions v0.1.11 to v0.1.26 (inclusive)
introduced in commit 62b1ce8e28bef49bd6b31cbd6405bd0385719a26.

static/js/webrtc.js
@@ -441,9 +441,7 @@ const rtc = (() => {
       });
     },
     getUserId: () => self._pad.getUserId(),
-    hangup: (...args) => {
-      const userId = args[0];
-      const notify = args[1] || true;
+    hangup: (userId, notify = true) => {
       if (!pc[userId]) return;
       self.setStream(userId, null);
       pc[userId].close();