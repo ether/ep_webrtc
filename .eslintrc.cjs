f6726f48a395b6ac5d09a7475dd5fc9b82bc597d
Richard Hansen <rhansen@rhansen.org>
2022-02-26 02:11:21 -0500

    autofixes from Etherpad checkPlugin.js

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+'use strict';
+
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('eslint-config-etherpad/patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: 'etherpad/plugin',
+  ignorePatterns: [
+    '/static/js/adapter.js',
+  ],
+};
